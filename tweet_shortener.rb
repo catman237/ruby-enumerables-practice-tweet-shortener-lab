ffc5dd4e4bfa997fca9170f736f7597909c506fd
Greg <Ghughes610@gmail.com>
2021-06-09 17:16:54 -0600

    commit tree c8054f3babbc41bcd9cf1eb0d75e1c9a31f22027

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require "pry"
+
+def dictionary 
+        dictionary = {  
+    "hello" => "hi",
+    ["to", "two", "too"] => "2",
+    ["for", "four"] => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@" ,
+    "and" => "&"
+    }
+end
+
+def word_substituter tweet
+    new_words = tweet.split.collect do  |word|
+        if dictionary.keys.include?(word)
+            word = dictionary[word]
+        else
+            word
+        end 
+    end.join(" ")
+    binding.pry
+end
+
+
+
